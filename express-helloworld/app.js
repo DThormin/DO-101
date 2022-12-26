d61dbaaa1c7d46f92ddc33f7cea815e97d0dc96b
Daniel Araujo Tormin <daniel.tormin@goinfra.go.gov.br>
2022-12-26 14:41:02 -0300

    teste openshift

---
+++ express-helloworld/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+});
+
+//Para implementar o novo recurso, adicione as seguintes linhas ao app.jsarquivo:
+
+app.get('/mars', function(req, res) {
+  res.send('Olá Marte!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
